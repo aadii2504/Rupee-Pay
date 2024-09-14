bd7817e8978a68c512f13a0521a8475ee384265c
Aadii2504 <adityadhaygude25@gmail.com>
2024-09-14 12:03:57 +0530

    JWT verified

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,82 +1,88 @@
 const express = require("express");
-const zod  =  require("zod")
-const {User} = require("../db")
-const jwt =  require("jsonwebtoken")
+const zod = require("zod");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const JWT_SECRET = require("../config")
+const JWT_SECRET = require("../config");
 
-const signupSchema   =  zod.object({
-    username: zod.string(),
-    firstName: zod.string(),
-    password: zod.string(),
-})
+const signupSchema = zod.object({
+  username: zod.string(),
+  firstName: zod.string(),
+  password: zod.string(),
+});
 
-/// signup and signin routes 
+/// signup and signin routes
 
-router.post("/signup" , async (req , res)=> {
-    const body =  req.body;
-    const {success} = signupSchema.safeParse(req.body);
-    if(!success) {
-        return res.json({
-            message: "Email already exists / Incorrect Input",
-        })
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const { success } = signupSchema.safeParse(req.body);
+  if (!success) {
+    return res.json({
+      message: "Email already exists / Incorrect Input",
+    });
+  }
+
+  const user = User.findOne({
+    username: body.username,
+  });
+
+  if (user._id) {
+    return res.json({
+      message: "Email already exists / Incorrect Input",
+    });
+  }
+
+  await User.create(body);
+  const token = jwt.sign(
+    {
+      userId: dbUser._id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    message: "User Created Successfully",
+    token: token,
+  });
+});
+
+router.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username asd Password is Required",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
     }
 
-    const user = User.findOne({
-        username: body.username
+    if (user.password != password) {
+        return res.status(400).json({
+          message: "Invalid Credentials",
+        });
+    }
+
+    const token  = jwt.sign({userId: user._id} , JWT_SECRET, {
+        expiresIn: '1h'
     })
-
-    if(user._id) {
-        return res.json({
-            message: "Email already exists / Incorrect Input"
-        })
-    }
-
-
-    await User.create(body);
-    const token =  jwt.sign({
-        userId: dbUser._id
-    } , JWT_SECRET);
 
     res.json({
-        message: "User Created Successfully",
-        token : token
-    })
+        message:"User Signed Succssfull"
+    });
 
 
-})
-
-
-router.post("/signin" , async (req ,res) => {
-   const  {username ,  password} =  req.body;
-     if(!username  || !password) {
-        return res.status(400).json({
-            message: "Username asd Password is Required";
-        })
-     }
-
-
-     try{
-        const user = await User.findOne({username})
-
-        if(!user) {
-            return res.status(400).json({
-                message: "Invalid Credentials"
-            })
-
-            if(user.password !=  password) {
-                return res.status(400).json({
-                    message: "Invalid Credentials"
-                })
-            }
-        }
-
-     } catch(err) {
-        return res.status(400).json({
-            message: "Internal Error is Occured"
-        })
-     }
-})
+  } catch (err) {
+    return res.status(400).json({
+      message: "Internal Error is Occured",
+    });
+  }
+});
 
 module.exports = router;
-
