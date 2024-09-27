768a344ff338ef52f54f07ed6030b550c9bad883
Aadii2504 <adityadhaygude25@gmail.com>
2024-09-28 00:01:34 +0530

    Account Schema Updated

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,61 @@
 const mongoose = require("mongoose");
+const { string } = require("zod");
 
 
 mongoose.connect("mongodb+srv://aditya:4adi2504@cluster0.guwptrv.mongodb.net/");
 
 const userSchema =  mongoose.Schema({
-    username: String,
-    password: String,
-    firstname: String,
-    lastname: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim : true,
+        lowercase : true,
+        minlength: 3,
+        maxlength : 30
+
+    },
+    password: {
+        type: String, 
+        required: true,
+        minLength : 6
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim : true ,
+        maxLength : 30
+    },
+    lastname:  {
+        type : String,
+        required : true ,
+        trim : true,
+        maxLength : 30
+    }
+})
+ 
+
+
+const accountSchema  = new mongoose.Schema({
+    userId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+
+    },
+    balance :{
+        type: Number,
+        required : true
+    }
+
 })
 
+
+const Account = mongoose.model('Account' , accountSchema)
 const User = mongoose.model("User" ,  userSchema);
 
 module.export = {
-    User
+    User,
+    Account
 }
 
