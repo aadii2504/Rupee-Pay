6a4031a719a9c5a767d30a32efbb8ecfa9d59475
Aadii2504 <adityadhaygude25@gmail.com>
2024-09-28 22:45:37 +0530

    Balance updated

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -2,3 +2,20 @@ const express = require('express')
 const{ authmiddleware }  = require("../middleware") 
 const { Account } = require("../db")
 const { default : mongoose}   =  require("mongoose")
+
+const router = express.Router();
+
+router.get("/balance" , authmiddleware , async (req ,  res) => {
+        const account   = await Account.findOne({
+            userId: req.userId
+        })
+
+        res.json({
+            balance: account.balance
+        })
+})
+
+
+router.post("/", authmiddleware , async(req, res) => {
+     
+})
