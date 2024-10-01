a0b6e8972a28ec402c2118932e31bc8135b4e5b5
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-01 22:51:34 +0530

    Balance comp created

---
+++ frontend/src/components/Balance.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div className='flex'>
+        <div className='font-bold text-lg'>
+            Your Balance 
+        </div> 
+        <div className='font-semibold ml-4 text-lg'>
+            RS {value}
+        </div>
+
+    </div>
+  )
+}
+
+export default Balance
