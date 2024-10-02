a9e74ab2c86e59fe912896e5a75187c95f5987cd
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-03 00:04:16 +0530

    User Finalised

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "./Button"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -33,6 +36,28 @@ const Users = () => {
 };
 
 
-function
+function User({user}) {
+  const  navigate = useNavigate();
+  return <div className="flex justify-between">
+    <div className="flex">
+      <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+        <div className="flex flex-col justify-center h-full text-xl">
+            {user.firstName[0]}
+        </div>
+      </div>
+      <div className="flex flex-col justify-center h-full">
+          <div>
+            {user.firstName} {user.lastName}
+          </div>
+      </div>
+    </div>
+    <div className="flex flex-col justify-center h-full">
+          <Button onClick={(e) => {
+            navigate("/send?id=" + user._id + "&name=" + user.firstName);
+
+          }} label={"Send Money"}/> 
+    </div>
+  </div>
+}
 
 export default Users;
