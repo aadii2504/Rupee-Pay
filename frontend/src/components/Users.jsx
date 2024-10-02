1d9d8e18854aa98c3fefe1909118d21fe0b4e9ee
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-02 23:54:13 +0530

    User  integrated

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -18,8 +18,15 @@ const Users = () => {
     <div className='font-bold mt-6 text-lg'>
         Users
     </div> 
+    <div className='my-2'>
+        <input onChange={(e) => {
+          setFilter(e.target.value)
+        }}  type='text' placeholder='Search users...' className='w-full px-2 py-1 border rounded border-slate-200'/>
+
+    </div>
     <div>
-        
+      {users.map(user =>  <User user={user}/> )}
+      
     </div>
     </>
     
