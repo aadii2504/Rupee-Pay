4b42536366381be90a0332c7b87554bd7430ce24
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-02 23:55:03 +0530

    User  updated

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
 
-    const [users , setUsers] = useState([]);
-    const [filter , setFilter] = useState("");
+  useEffect(() => {
+    axios.get("" + filter).then((response) => {
+      setUsers(response.data.user);
+    });
+  }, [filter]);
 
-    useEffect(() => {
-        axios.get("" + filter )
-        .then(response => {
-            setUsers(response.data.user)
-        })
-    }, [filter])
-
-
-  return ( 
+  return (
     <>
-    <div className='font-bold mt-6 text-lg'>
-        Users
-    </div> 
-    <div className='my-2'>
-        <input onChange={(e) => {
-          setFilter(e.target.value)
-        }}  type='text' placeholder='Search users...' className='w-full px-2 py-1 border rounded border-slate-200'/>
-
-    </div>
-    <div>
-      {users.map(user =>  <User user={user}/> )}
-      
-    </div>
+      <div className="font-bold mt-6 text-lg">Users</div>
+      <div className="my-2">
+        <input
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+          type="text"
+          placeholder="Search users..."
+          className="w-full px-2 py-1 border rounded border-slate-200"
+        />
+      </div>
+      <div>
+        {users.map((user) => (
+          <User user={user} />
+        ))}
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Users
+
+function
+
+export default Users;
