aba7a59f545e33fd9f899a60a067e5a085c202f1
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-05 17:35:53 +0530

    SignIn & signUp page integrated
    solved object de structuring issue and initialized tailwind css  imported comps in main and index.css and app.css contain  removed and index contain tailwind comps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,24 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import SendMoney from "./pages/SendMoney";
+import {Signin} from "./pages/Signin";
+import{ Signup} from "./pages/Signup";
+import {Dashboard} from "./pages/Dashboard";
+import {SendMoney }from "./pages/SendMoney";
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={Signin}/>
-        <Route path="/signup" element={Signup}/>  
-        <Route path="/dashboard" element={Dashboard}/>
-        <Route path="send" element={SendMoney}/>  
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>  
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="send" element={<SendMoney/>}/>  
       </Routes>
       </BrowserRouter>
     </>
