f6d6a15d5fb8c76f5c3785401880c759c5f81952
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-05 12:44:55 +0530

    For object destructuring

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Heading = ({ label }) => {
+export function Heading({ label }) {
   return <div className="font-bold text-4xl pt-6">{label}</div>;
 };
 
-export default Heading;
+ 
--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const InputBox = ({label , placeholder , onChange}) => {
+export function InputBox({label , placeholder , onChange}){
   return (
     <div>
       <div className='text-sm font-medium text-left py-2'>
@@ -11,4 +11,4 @@ const InputBox = ({label , placeholder , onChange}) => {
   )
 }
 
-export default InputBox
+
