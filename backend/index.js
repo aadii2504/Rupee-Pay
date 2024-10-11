9c72e1801415932833b250c2c7de32de37ae86e8
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-11 14:30:23 +0530

    sequence set

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
-const  express = require("express");
-const cors  =  require("cors");
+// backend/index.js
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const mainRouter = require("./routes/index");
+app.use("/api/v1", rootRouter);
 
-
-
-app.use("/api/v1" , mainRouter);
 app.listen(3000);
-
