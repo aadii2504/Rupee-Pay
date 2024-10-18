73cbc02a1f3df70e6a564b23ff2384e9fdc17f4d
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-18 22:06:05 +0530

    without router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,15 +15,15 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+
       <Routes>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>  
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/send" element={<SendMoney/>}/>  
       </Routes>
-      </BrowserRouter> 
-      {/* completed */}
+  
+      
     </>
   )
 }
